0617a208ebb208dc98c28841bff58475474f1ad9
Daehyun Kim <hatemogi@gmail.com>
2015-12-23 12:29:33 +0900

    Ring 최초 핸들러 추가

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject ring-study "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "클로저 Ring 학습"
+  :url "https://github.com/hatemogi/ring-study"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
   :main ^:skip-aot ring-study.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ring_study/core.clj
+++ src/ring_study/core.clj
@@ -1,7 +1,6 @@
-(ns ring-study.core
-  (:gen-class))
+(ns ring-study.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
